fb5446162beec858b19f0db29d5473eec433ef2b
Wwj <575413642@qq.com>
2017-06-16 17:57:02 +0800

    add shopping cart

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -2,13 +2,36 @@
 var util = require('../../utils/util.js')
 Page({
   data: {
-    logs: []
+    order:[{
+      no_expired: [{
+        img: 'http://demoshop.youpzt.com/uploads/product/2017-03-01/58b6a02c36f0b.jpg',
+        title: '33333333333333333333333white-space: pre-wrap;',
+        small: '444444444444444',
+        money: '555555'
+      }, {
+        img: 'http://demoshop.youpzt.com/uploads/product/2017-03-01/58b6a02c36f0b.jpg',
+        title: '33333333333333333333333white-space: pre-wrap;',
+        small: '444444444444444',
+        money: '555555'
+        }, {
+          img: 'http://demoshop.youpzt.com/uploads/product/2017-03-01/58b6a02c36f0b.jpg',
+          title: '33333333333333333333333white-space: pre-wrap;',
+          small: '444444444444444',
+          money: '555555'
+      }, {
+        img: 'http://demoshop.youpzt.com/uploads/product/2017-03-01/58b6a02c36f0b.jpg',
+        title: '33333333333333333333333white-space: pre-wrap;',
+        small: '444444444444444',
+        money: '555555'
+      },],
+      expired:{}
+    }]
   },
   onLoad: function () {
-    this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(function (log) {
-        return util.formatTime(new Date(log))
-      })
-    })
+    // this.setData({
+    //   logs: (wx.getStorageSync('logs') || []).map(function (log) {
+    //     return util.formatTime(new Date(log))
+    //   })
+    // })
   }
 })
